5811043306a68c6ea5f070f3cd06fb9ea2bf5a04
pavitagrawal <148257252+pavitagrawal@users.noreply.github.com>
2024-09-10 02:55:53 +0530

    Update stack_bases.c

--- 230953406 DS Lab/Lab 3/stack_bases.c
+++ 230953406 DS Lab/Lab 3/stack_bases.c
@@ -1,41 +1,48 @@
-#include<stdio.h>
+#include <stdio.h>
 #define MAX 100
+
 int stack[MAX];
- struct Stack s;
-int PUSH(int *top, int item){
-    if(*top==MAX-1){
+
+int PUSH(int *top, int item) {
+    if (*top == MAX - 1) {
         printf("Stack full\n");
+        return -1;
     }
-    stack[++*top]=item;
+    stack[++(*top)] = item;
+    return 0;
 }
-int POP(int *top){
-    if(*top==-1){
+
+int POP(int *top) {
+    if (*top == -1) {
         printf("Stack underflows\n");
-        return-1;
+        return -1;
     }
     return stack[(*top)--];
 }
-int main(){
-    int num, top=-1, i, rem, base, t;
+
+int main() {
+    int num, top = -1, rem, base, t;
     printf("Enter the number: ");
     scanf("%d", &num);
     printf("Enter the base: ");
     scanf("%d", &base);
     t = num;
-    while(t!=0){
-        rem = t%base;
-        stack[100]=PUSH(&top, rem);
-        t=t/base;
+
+    while (t != 0) {
+        rem = t % base;
+        PUSH(&top, rem);
+        t = t / base;
     }
-    int digit = pop(&s);
+
+    printf("Answer: ");
+    while (top != -1) {
+        int digit = POP(&top);
         if (digit >= 10) {
             printf("%c", 'A' + (digit - 10));
         } else {
             printf("%d", digit);
         }
-    printf("Answer: ");
-    while(top!=-1){
-        t = POP(&top);
-        printf("%d", t);
     }
+    printf("\n");
+    return 0;
 }
