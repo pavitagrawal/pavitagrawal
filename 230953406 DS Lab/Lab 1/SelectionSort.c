8103c870e31bc910e7b5ec4ccd1d4e6301fd1c79
pavitagrawal <148257252+pavitagrawal@users.noreply.github.com>
2024-09-10 01:21:37 +0530

    Update SelectionSort.c

--- 230953406 DS Lab/Lab 1/SelectionSort.c
+++ 230953406 DS Lab/Lab 1/SelectionSort.c
@@ -1,20 +1,23 @@
 #include<stdio.h>
 int main(){
-    int n, arr[50], i, j, temp;
+    int n, arr[50], i, j, pos, small;
     printf("Enter the no. of elements of list: ");
     scanf("%d", &n);
     printf("Enter the elements: ");
     for(i=0; i<n; i++){
         scanf("%d", &arr[i]);
     }
-    for(i=0;i<n-1;i++){
+    for(i=0;i<n;i++){
+        pos = i;
+        small = arr[i];
         for(j=i+1;j<n;j++){
-            if(arr[i]>arr[j]){
-                temp = arr[j];
-                arr[j] = arr[i];
-                arr[i] = temp;
+            if(small>arr[j]){
+                pos = j;
+                small = arr[j];
             }
         }
+        arr[pos]=arr[i];
+        arr[i]=small;
     }
     printf("The new list is: ");
     for(i=0; i<n; i++){
