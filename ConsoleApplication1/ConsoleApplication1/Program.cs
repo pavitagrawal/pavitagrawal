f7baefc249d7a63fb4ec67c673bff8db81ddf893
pavitagrawal <148257252+pavitagrawal@users.noreply.github.com>
2025-02-04 16:25:10 +0530

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String message = "Hello World";
+            Console.WriteLine(message);
+            Console.ReadLine();
+        }
+    }
+}
